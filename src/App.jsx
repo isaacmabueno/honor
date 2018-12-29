3e89d2c3798c8af839be0aa7d3a9079b06f5c479
isaacmabueno <isaacmabueno@gmail.com>
2018-12-28 20:19:57 -0600

    getting pages working and starting on nav

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Home from './components/Home';
 import About from './components/About';
 import News from './components/News';
 import Contact from './components/Contact';
+import CustomNavbar from './components/CustomNavbar'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <CustomNavbar />
           <Route exact path='/' component={Home} />
           <Route path='/about' component={About} />
           <Route path='/news' component={News} />
