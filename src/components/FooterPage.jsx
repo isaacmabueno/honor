08a7159b538b3eaca58384a63ef5e4010070b0e7
isaacmabueno <isaacmabueno@gmail.com>
2019-01-20 13:18:51 -0600

    importing new fonts and updating footer and home components with new content

--- src/components/FooterPage.jsx
+++ src/components/FooterPage.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Footer, Nav, NavItem, Image } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css';
 import './FooterPage.scss';
 
@@ -15,15 +14,15 @@ class FooterPage extends React.Component {
           </div>
           <div className="col-md-4">
             <ul>
-              <li>Chicago, IL 60622</li>
-              <li>Email: <a href="hello@honoragency.com">hello@honoragency.com</a></li>
+              <li>Chicago, IL</li>
+              <li>Email: <a href="mailto:hello@honoragency.com">hello@honoragency.com</a></li>
             </ul>
             
           </div>
           <div className="col-md-4">
             <ul className="center">      
               <li>&copy; Honor Agency 2019</li>
-              <li className='small'>Made with <i className="fa fa-heart"></i> in Chicago</li>
+              <li className='small'>Made with <i className="fa fa-coffee"></i> in Chicago</li>
             </ul>
           </div>
         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,15 +12,16 @@ export default class Home extends Component {
                     <Col md={12}>
                         <h2 className='text-center'>Two to design for you!</h2>
                     </Col>
-                    <Col xs={6} xsOffset={3}>
-                        <p className='text-center'>orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliq</p>
+                    <Col xs={4} xsOffset={4}>
+                        <p className='text-center'>A husband and wife design and development shop. </p>
+                        <p>We create interactive experiences through beautiful and continual design</p>
                     </Col>
                 </Row>
 
                 <Row className='client-listing'>
                     <Col md={6}>
                     <figure class="overlay">
-                        <Image src='assets/rubens.jpg' />
+                        <Image src='assets/rubens.jpg' alt='Rubens Unisex' />
                         <figcaption>
                             <h3>View Project</h3>
                         </figcaption>
@@ -29,28 +30,25 @@ export default class Home extends Component {
                     </Col>
                     <Col md={6}>
                     <figure class="overlay">
-                        <Image src='assets/DOOM.jpg' />
+                        <Image src='assets/DOOM.jpg' alt="Impending Doom Music" />
                         <figcaption>
                             <h3>View Project</h3>
                         </figcaption>
                         <a href="#"></a>
                     </figure>
                     </Col>
-                    
                     <Col md={6}>
                     <figure class="overlay">
-                        <Image src='assets/ptfa.jpg' />
+                        <Image src='assets/ptfa.jpg' alt="Present Tense Fine Ales" />
                         <figcaption>
                             <h3>View Project</h3>
                         </figcaption>
                         <a target='_blank' href="http://www.presenttensefineales.com"></a>
                     </figure>
                     </Col>
-                    
-                    
                     <Col md={6}>
                     <figure class="overlay">
-                        <Image src='assets/trecutr.jpg' />
+                        <Image src='assets/trecutr.jpg' alt='Aesthetic Denver' />
                         <figcaption>
                             <h3>View Project</h3>
                         </figcaption>
